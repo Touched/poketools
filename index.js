885e226c37d92b10e9a8154536728f9aebf0fe8a
Touched <jtouched@gmail.com>
2015-05-07 23:31:56 +0200

    Map data

--- index.js
+++ index.js
@@ -3,35 +3,34 @@ var tileset = require('./lib/map/tileset');
 var PNG = require('node-png').PNG;
 
 var buffer = fs.readFileSync('./BPRE0.gba');
-var data = tileset.load(buffer, 0x2D4A94);
+var data = tileset.load(buffer, 0x2D4A94 + 0x18);
 
-function writeLayer(layer, tileWidth) {
-    tileWidth = tileWidth || 5;
-
+function writeLayer(data) {
     var tilesetImage = new PNG({
         filterType: 4,
-        width: 16 * tileWidth,
-        height: 0x280 / tileWidth * 16
+        width: 16 * data.length,
+        height: 16 * 2
     });
 
-    data.forEach(function (block, n) {
-        var image = new PNG({
-            filterType: 4,
-            width: 16,
-            height: 16
+    for (var layer = 0; layer < 2; ++layer) {
+        data.forEach(function (block, n) {
+            var image = new PNG({
+                filterType: 4,
+                width: 16,
+                height: 16
+            });
+
+            for (var i = 0; i < 1; ++i) {
+                image.data = block[layer];
+            }
+
+            image.data = new Buffer(image.data);
+
+            image.bitblt(tilesetImage, 0, 0, 16, 16, n * 16, layer * 16);
         });
+    }
 
-        for (var i = 0; i < 1; ++i) {
-            image.data = block[layer];
-        }
-
-        image.data = new Buffer(image.data);
-
-        image.bitblt(tilesetImage, 0, 0, 16, 16, (n % tileWidth) * 16, Math.floor(n / tileWidth) * 16);
-    });
-
-    tilesetImage.pack().pipe(fs.createWriteStream('out/layer2' + layer + '.png'));
+    tilesetImage.pack().pipe(fs.createWriteStream('out/tileset.png'));
 }
 
-writeLayer(0, 10);
-writeLayer(1, 10);
+writeLayer(data);
